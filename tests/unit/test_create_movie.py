1dd19bdc5a7f34b1fabd22693f6a77eda470104a
thompaw <98345171+thompaw@users.noreply.github.com>
2022-03-14 22:18:47 -0400

    changed variable names in create_movie, created unit and e2e test cases for the create_movie function. suffered.

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,9 +1,29 @@
 # TODO: Feature 2
-import app
+from pyparsing import empty
+from app import create_movie
+from src.repositories.movie_repository import movie_repository_singleton
 
+def test_createMovieFn():
+    mov_list = movie_repository_singleton.get_all_movies()
+    # good
+    create_movie('titletest', 'directortest', '5') # good input, everything within bounds
 
-def test_create_movie():
-    # good path
-    app.create_movie()
-    # bad path
+    # check that movie has been added
+    assert mov_list is not empty
+
+    # check individual aspects of the movie
+    assert mov_list[0].title == 'titletest'
+    assert mov_list[0].director == 'directortest'
+    assert mov_list[0].rating == '5'
+    # clean between takes
+    mov_list.clear()
+    # bad 
+    create_movie('AAAAAAA', '', '0')
+
+    # check that movie has been added
+    assert mov_list is not empty
+
+    assert mov_list[0].title == 'AAAAAAA'
+    assert mov_list[0].director == ''
+    assert mov_list[0].rating == '1'
 
